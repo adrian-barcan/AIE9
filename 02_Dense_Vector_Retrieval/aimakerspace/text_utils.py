1123b931768d01f7e0751c126d0234f31bceaeb8
adrianbarcan <adrian.barcan1012@gmail.com>
2026-01-19 21:28:50 +0200

    Week 1 - Assignement 2

--- 02_Dense_Vector_Retrieval/aimakerspace/text_utils.py
+++ 02_Dense_Vector_Retrieval/aimakerspace/text_utils.py
@@ -1,5 +1,6 @@
 import os
 from typing import List
+from pypdf import PdfReader
 
 
 class TextFileLoader:
@@ -30,6 +31,50 @@ class TextFileLoader:
                         os.path.join(root, file), "r", encoding=self.encoding
                     ) as f:
                         self.documents.append(f.read())
+
+    def load_documents(self):
+        self.load()
+        return self.documents
+
+
+class PDFFileLoader:
+    def __init__(self, path: str):
+        self.documents = []
+        self.path = path
+
+    def load(self):
+        if os.path.isdir(self.path):
+            self.load_directory()
+        elif os.path.isfile(self.path) and self.path.endswith(".pdf"):
+            self.load_file()
+        else:
+            raise ValueError(
+                "Provided path is neither a valid directory nor a .pdf file."
+            )
+
+    def load_file(self):
+        reader = PdfReader(self.path)
+        text_parts = []
+        for page in reader.pages:
+            page_text = page.extract_text()
+            if page_text:
+                text_parts.append(page_text)
+        full_text = "\n".join(text_parts)
+        self.documents.append(full_text)
+
+    def load_directory(self):
+        for root, _, files in os.walk(self.path):
+            for file in files:
+                if file.endswith(".pdf"):
+                    file_path = os.path.join(root, file)
+                    reader = PdfReader(file_path)
+                    text_parts = []
+                    for page in reader.pages:
+                        page_text = page.extract_text()
+                        if page_text:
+                            text_parts.append(page_text)
+                    full_text = "\n".join(text_parts)
+                    self.documents.append(full_text)
 
     def load_documents(self):
         self.load()
